f7cd5c518a9f905a5dd95a227851f6b9a96a157e
DaNike <danike@cirr.com>
2025-08-10 18:10:32 -0500

    Change ARM64 classifier to just use AMD64 classifier

--- src/MonoMod.Core/Platforms/Systems/SystemVABI.cs
+++ src/MonoMod.Core/Platforms/Systems/SystemVABI.cs
@@ -18,10 +18,7 @@ namespace MonoMod.Core.Platforms.Systems
                 if (totalSize > 32)
                     return isReturn ? TypeClassification.ByReference : TypeClassification.OnStack;
 
-                var isMemory = SysVIsMemoryCache.GetValue(
-                    type,
-                    static t => new StrongBox<bool>(AnyFieldsNotFloat(t))
-                ).Value;
+                var isMemory = AnyFieldsNotFloat(type);
                 if (isMemory)
                 {
                     return isReturn ? TypeClassification.ByReference : TypeClassification.OnStack;
@@ -30,44 +27,50 @@ namespace MonoMod.Core.Platforms.Systems
             return TypeClassification.InRegister;
         }
 
-        public static TypeClassification ClassifyARM64(Type type, bool isReturn)
-        {
-            // ARM64 uses a byref ret buf in x8 if the return size is > 16, but there is no way to model this
-            // currently so just return InRegister for now so the return buffer fixup is effectively disabled
-            if (!isReturn)
-            {
-                var totalSize = type.GetManagedSize();
-                if (totalSize > 16)
-                {
-                    if (totalSize > 32)
-                        return TypeClassification.OnStack;
+        public static TypeClassification ClassifyARM64(Type type, bool isReturn) => ClassifyAMD64(type, isReturn);
+        //{
+        //    // ARM64 uses a byref ret buf in x8 if the return size is > 16, but there is no way to model this
+        //    // currently so just return InRegister for now so the return buffer fixup is effectively disabled
+        //    if (!isReturn)
+        //    {
+        //        var totalSize = type.GetManagedSize();
+        //        if (totalSize > 16)
+        //        {
+        //            if (totalSize > 32)
+        //                return TypeClassification.OnStack;
 
-                    var isMemory = SysVIsMemoryCache.GetValue(
-                        type,
-                        static t => new StrongBox<bool>(AnyFieldsNotFloat(t))
-                    ).Value;
-                    if (isMemory)
-                    {
-                        return TypeClassification.OnStack;
-                    }
-                }
-            }
+        //            var isMemory = SysVIsMemoryCache.GetValue(
+        //                type,
+        //                static t => new StrongBox<bool>(AnyFieldsNotFloat(t))
+        //            ).Value;
+        //            if (isMemory)
+        //            {
+        //                return TypeClassification.OnStack;
+        //            }
+        //        }
+        //    }
 
-            return TypeClassification.InRegister;
-        }
+        //    return TypeClassification.InRegister;
+        //}
+
+        private static readonly StrongBox<bool> SBTrue = new(true);
+        private static readonly StrongBox<bool> SBFalse = new(false);
 
         private static bool AnyFieldsNotFloat(Type type)
         {
-            foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            return SysVIsMemoryCache.GetValue(type, static type =>
             {
-                var fieldType = field.FieldType;
-                if (fieldType is { IsPrimitive: false, IsValueType: true } && AnyFieldsNotFloat(fieldType))
-                    return true;
-                if (Type.GetTypeCode(fieldType) is not TypeCode.Single and not TypeCode.Double)
-                    return true;
-            }
+                foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                {
+                    var fieldType = field.FieldType;
+                    if (fieldType is { IsPrimitive: false, IsValueType: true } && AnyFieldsNotFloat(fieldType))
+                        return SBTrue;
+                    if (Type.GetTypeCode(fieldType) is not TypeCode.Single and not TypeCode.Double)
+                        return SBTrue;
+                }
 
-            return false;
+                return SBFalse;
+            }).Value;
         }
 
     }
